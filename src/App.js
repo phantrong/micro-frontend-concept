d5274a51de49fb237b0c6883f3854fd72fc86d74
phantrong001 <phantrong001@gmail.com>
2022-05-03 21:49:54 +0700

    add-content-text

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import { Router } from "react-router-dom";
 import "./App.css";
 import { createBrowserHistory } from 'history';
 
+import deployment from './deployment.png';
+
 const defaultHistory = createBrowserHistory();
 
 function App(props) {
   return (
     <Router history={props.history || defaultHistory}>
         <div className="content-page">
-          <h1>Khái niệm</h1>
+          <h2>Các khái niệm</h2>
+          <h3>Khái niệm microservices</h3>
           <p>
-          Good frontend development is hard. Scaling frontend development so that many teams can work simultaneously on a large and complex product is even harder. In this article we'll describe a recent trend of breaking up frontend monoliths into many smaller, more manageable pieces, and how this architecture can increase the effectiveness and efficiency of teams working on frontend code. As well as talking about the various benefits and costs, we'll cover some of the implementation options that are available, and we'll dive deep into a full example application that demonstrates the technique.
+          Theo cách mô tả đơn giản và ngắn gọn nhất về microservices, nó là một cách phát triển cho phép các developers develop và deploy độc lập các thành phần của một platform mà không làm ảnh hưởng tới các thành phần khác. Microservices cung cấp khả năng triển khai các module độc lập cho phép chúng ta xây dựng các service tách biệt và dễ dàng thay đổi. Nguyên tắc hàng đầu khi triển khai microservices là: mỗi service chỉ có một nhiệm vụ duy nhất và đảm bảo là nó không quá bự.
           </p>
+          <h3>Khái niệm micro frontend</h3>
+          <p>
+          Ý tưởng đằng sau cụm từ micro frontends là sự phân chia web app ra thành nhiều nhóm thành phần riêng biệt và được phát triển bởi các team độc lập, mỗi team có một nhiệm vụ khác nhau. Khái niệm micro frontend được lấy cảm hứng và được đặt tên theo microservices.
+          </p>
+          <img src={'http://localhost:3001' + deployment} alt="deployment"/>
         </div>
     </Router>
   );
